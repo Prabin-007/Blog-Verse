ee3ff35651fdc651faa50d8aa9b3b36918fd64ce
Prabin-007 <prabinprasad08@gmail.com>
2026-07-09 01:11:40 +0530

    fix casing

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 export default api;
